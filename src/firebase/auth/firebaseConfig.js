91826b2c28ec00b4a30d2ec7a0791938389a05ea
jackson mwatuka <jacksonymwatuka@gmail.com>
2022-07-06 09:35:18 +0300

    new app sales

---
+++ src/firebase/auth/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDw_ls75IwBSacLWwn2swt9uwCBsha7uFs",
+  authDomain: "sells-app-e538c.firebaseapp.com",
+  projectId: "sells-app-e538c",
+  storageBucket: "sells-app-e538c.appspot.com",
+  messagingSenderId: "869048852385",
+  appId: "1:869048852385:web:151e744fb85fb2c087b5e0"
+};
